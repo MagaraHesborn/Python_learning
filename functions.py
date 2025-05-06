e96b777eecf6d4e78ff4975a1b524d43e9771867
Magara Hesborn <magarahesborn1@gmail.com>
2025-05-06 20:51:34 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+#syntax
+
+def greet(name):
+    return f"Hello, {name}!"
+
+name = input("Enter your name: ")
+print(greet(name))
+
+def is_even(number):
+    return number % 2 == 0
+
+number = len(name)
+print(f"{number} is even: {is_even(number)}")
+
