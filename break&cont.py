aad0f4e42bb2892c628c54f701a17746c998f770
Magara Hesborn <magarahesborn1@gmail.com>
2025-05-06 20:23:12 +0300

    break and continue

---
+++ break&cont.py
@@ -0,0 +1,11 @@
+chores = ["dishes", "laundry", "vacuum", "mop", "dust", "trash", "windows", "groceries", "bathroom", "yardwork"]
+
+for chore in chores:
+    if chore == "vacuum":
+        continue
+    elif chore == "groceries":
+        break
+    print(chore)
+print("All chores compeleated")
+    #the code above is a simple implementation of break and continue statements in a for loop.
+    
