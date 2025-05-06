e8e49f733bedb12388edf016c55239faf5f3c728
Magara Hesborn <magarahesborn1@gmail.com>
2025-05-06 16:28:14 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+price = 11.54
+name = "Hesborn"
+quantity = 5
+in_stock = True
+
+#type checking for variables
+
+print(type(price))
+print(type(name))
+print(type(quantity))
+print(type(in_stock))
+
+#mini program that stores my data and prints it out
+
+age = 26
+height = 177
+programmer = True
+
+print(f"Hello, my name is {name}. I am {age} years old and {height} cm tall. Am I a programmer? {programmer}.")
